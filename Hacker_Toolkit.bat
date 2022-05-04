48935ee2f8093c7c2f557ecf31ec2a3068be7f5d
Christopher Mills <github@chrisam.net>
2022-05-04 10:52:48 -0700

    Create Hacker_Toolkit.bat

---
+++ Hacker_Toolkit.bat
@@ -0,0 +1,516 @@
+@echo off
+:menu
+title Hacking Tools
+color D
+cls
+echo                                                                                                        /====================================\
+echo                                                                                                       /                BY COZI               \
+echo                                                                                                      / Welcome to the Ultimate Hacker Toolkit \
+echo                                                                                                      \          Use at Your Own Risk          /
+echo                                                                                                       \======================================/
+pause
+color C
+:stuff
+echo.
+echo 1) Remote Shell Access
+echo 2) Remote Desktop Control
+echo 3) Password Recovery
+echo 4) DDos Attack
+echo 5) Ip Grabber
+echo 6) Hack Wifi Password
+echo 7) Remote Computer Shutdown
+echo 8) Encrpt/Decrypt Files
+echo 9) Hack Instagram Password
+echo 10) Hack Files
+echo 11) Hack Gmail Accounts
+set /p number=
+color 9
+if %number% == 1 goto Shell
+if %number% == 2 goto Desktop
+if %number% == 3 goto Password
+if %number% == 4 goto Ddosser
+if %number% == 5 goto Ip
+if %number% == 6 goto Wifi
+if %number% == 7 goto Shutdown
+if %number% == 8 goto Crypt
+if %number% == 9 goto Insta
+if %number% == 10 goto Ftp
+if %number% == 11 goto Gmail
+
+:Gmail
+title Gmail Hacker
+cls
+set /p gil=Hack Gmails?(y/n)
+if %gil% == y goto hail
+if %gil% == n goto menu
+
+:hail
+cls
+tree
+tree
+tree
+netstat -n
+timeout 3
+goto mat
+
+:mat
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ping localhost -n 1 >nul
+ipconfig /all
+ping localhost -n 2 >nul
+echo.
+echo            Passwords Successfully Hacked!
+echo      Check GmailPass Folder for Passwords.txt
+echo.
+pause
+goto menu
+
+:Ftp
+title Files Hacker
+cls
+echo Have you Ever Wanted to Access Someones Files?
+echo ---------------------------------------------
+pause
+echo.
+echo 1) Hack Random File Server
+echo 2) Hack a Victim's Files
+set /p server=
+if %server% == 1 goto ftp2
+if %server% == 2 goto pftp
+
+:ftp2
+set /a num=%random% %% 2
+if %num% == 0 goto ft
+if %num% == 1 goto ftpp
+if %num% == 2 goto ftpp
+if %num% == 3 goto ftpp
+if %num% == 4 goto ftpp
+if %num% == 5 goto ftpp
+if %num% == 6 goto ftpp
+if %num% == 7 goto ftpp
+if %num% == 9 goto ftpp
+if %num% == 10 goto ftpp
+
+:ft
+cls
+tasklist
+tree
+echo.
+echo Scanning Vulnerable Servers
+timeout 5
+echo.
+echo Credentials Found!
+echo  /******************************\
+echo / ---- Username: anonymous ----- \
+echo \ ----- Password: hkeysys ------ /
+echo  \******************************/
+start cmd /k ftp 90.130.70.73
+pause
+goto menu
+
+:ftpp
+cls
+tasklist
+tree
+echo.
+echo Scanning Vulnerable Servers
+timeout 5
+echo.
+echo Credentials Found!
+echo  /******************************\
+echo / ------- Username: demo ------- \
+echo \ ----- Password: password ----- /
+echo  \******************************/
+start cmd /k ftp 195.144.107.198
+pause
+goto menu
+
+:pftp
+cls
+set /p gg=Enter Victim's Ip: 
+start cmd /k ftp %gg%
+pause
+goto menu
+
+:Insta
+title Instagram Password Hacker
+color 0a
+title WoT Instagram Tool ^| 3.7
+cls
+echo Please enter the username of the account you would like to penetrate.
+set /p menu=
+echo.
+echo Beginning brute force of %menu%'s account shortly...
+echo Please be patient while the necessary databases are downloaded.
+ping localhost -n 5 >nul
+echo Database 1/5 finished...
+ping localhost -n 5 >nul
+echo Database 2/5 finished...
+ping localhost -n 5 >nul
+echo Database 3/5 finished...
+ping localhost -n 5 >nul
+echo Database 4/5 finished...
+ping localhost -n 5 >nul
+echo Database 5/5 finished...
+ping localhost -n 5 >nul
+echo Finishing...
+echo.
+ping localhost -n 10 >nul
+echo Complete!
+ping localhost -n 1 >nul
+goto startBruteforce
+:startBruteforce
+echo Attempting Passwords, please be patient...
+echo You will be notified when a password has been found.
+echo.
+echo NOTE: If WoT Instagram Tool 3.7 detects that the user has changed their password, the process will be aborted.
+set /p waiting=
+
+:Crypt
+title Crypter
+cls
+set /p huh=Encrypt or Decrypt?(e,d)
+if %huh% == e goto ecrpt
+if %huh% == d goto dcrypt
+
+:dcrypt
+cls
+set /p pathd=Enter Folder Path: 
+Cipher /d %pathd%
+pause
+goto menu
+ 
+:ecrpt
+cls
+set /p path=Enter Folder Path: 
+Cipher /e %path%
+pause
+goto menu
+
+:Ip
+cls
+set /p choice=Is the Victim on the Same Network as You?(y/n)
+if %choice% == y goto arp
+if %choice% == n goto net
+
+:net
+cls
+echo You Need to Either be on a Call or text the Victim when the Scan Starts
+echo              And Then You'll See Their Ip Address
+pause
+netstat
+goto menu
+
+:arp
+cls
+color A
+echo This will Show all the Ip Addresses on Your Network
+arp -a
+pause
+set /p advan=Show Advanced Info?(y/n)
+if %advan% == y goto adva
+if %advan% == n goto menu
+
+:adva
+cls
+cd %MYFILES%
+start WNetWatcher.exe
+goto menu
+
+:Shell
+title Remote Shell
+cls
+echo ------------- Welcome to Remote Shell ------------------
+echo Here You Will be Able to Access Someone's Command Prompt
+echo.
+echo 1) Hack Random Shell
+echo 2) Hack Victim's Shell
+set /p qwer=
+if %qwer% == 1 goto ssh
+if %qwer% == 2 goto victim
+
+:ssh
+cls
+start cmd /k ssh 80.112.159.205
+goto menu
+
+:victim
+cls
+echo is the Victim on a Different Network?(y/n)
+set /p shell1=
+if %shell1% == y goto difwif
+if %shell1% == n goto samewif
+
+:samewif
+cls
+color A
+set /p rand=Enter User's Private Ip Address: 
+set /p user=Enter Victim's Username: 
+echo Open Shell?(y\n)
+set /p shell2=
+if %shell2% == y goto shell3
+if %shell2% == n goto menu
+
+:difwif
+cls
+set /p rand=Enter Victim's Public Ip: 
+echo.
+set /p user=Enter Victim's Username: 
+echo Open Shell?(y\n)
+set /p shell2=
+if %shell2% == y goto shell3
+if %shell2% == n goto menu
+
+:shell3
+cls
+echo Connected!
+pause
+start cmd /k prompt C:\Users\%user%$G%
+goto menu
+
+:Desktop
+title Remote Desktop
+cls
+echo 1) Remote Control Random Computer
+echo 2) Remote Control Victim's Computer
+set /p rdp=
+if %rdp% == 1 goto rdp1
+if %rdp% == 2 goto rdp2
+
+:rdp1
+set /a num=%random% %% 2
+if %num% == 0 goto rdp3
+if %num% == 1 goto rdp4
+if %num% == 2 goto rdp4
+if %num% == 3 goto rdp4
+if %num% == 4 goto rdp4
+if %num% == 5 goto rdp4
+if %num% == 6 goto rdp4
+if %num% == 7 goto rdp4
+if %num% == 9 goto rdp4
+if %num% == 10 goto rdp4
+pause
+
+:rdp3
+cls
+echo Enter "35.163.62.219" in Connection Box
+echo Password: "Hh42lki123"
+start mstsc
+pause
+goto menu
+
+:rdp4
+cls
+echo Enter "154.127.53.121" in Connection Box
+echo Password: "Administrator"
+start mstsc
+pause
+goto menu
+
+:rdp2
+cls
+start mstsc.exe
+goto menu
+
+:Wifi
+Title Wifi Password Hack
+cls
+set /p show=Show Visited Wifi Networks?(y/n/exit): 
+if %show% == y goto wifi1
+if %show% == n goto wifi2
+if %show% == exit goto menu
+
+:wifi2
+Title Wifi Password Hack
+cls
+set /p intro=Have you Visited the Target Network before?(y/n)
+if %intro% == y goto wifi5
+if %intro% == n goto wifi6
+
+:wifi6
+cls
+echo [About Brute Forcer]
+echo	- This program performs an "Online Attack" to the target WI-FI.
+echo	- It's speed depends on your hardware.
+echo	-Not % 100 accurate because of it's a brute force attack.
+echo	- A big password file not included, you can create it with "keycombinator.cmd" or
+echo	you can download from internet and rename it "passlist.txt" and paste it inside "BF_Files"
+echo	- Your attack results are being written to "WIFI_Report.txt" inside "BF_Files"
+echo	- Please, do not touch other files, it may cause malfunction!
+echo	- "keycombinator.cmd" creates a list inside BF_Files with the name "passlist_raw.txt".
+echo	You have to rename it to "passlist.txt"
+echo.
+echo 1) Brute Force Network
+echo 2) Key Combinator
+set /p keyor=
+if %keyor% == 2 goto keycomb
+if %keyor% == 1 goto brutef
+
+:brutef
+cls
+cd %MYFILES%
+start bruteforce
+goto menu
+
+:keycomb
+cls
+cd %MYFILES%
+start keycombinator
+goto menu
+
+:wifi5
+cls
+set /p wifite=Enter Wifi Network: 
+netsh wlan show profiles %wifite% key=clear
+pause
+goto menu
+
+:wifi1
+Title Wifi Password Hack
+cls
+netsh wlan show profiles
+pause
+goto Wifi
+
+:Password
+cls
+title Password Recovery
+echo Welcome to Password Recovery
+pause
+cd %MYFILES%
+cd BF_Files
+Start recv.exe /stext
+echo ---------------------------------------
+echo Check PassRecovery Folder for Password
+echo ---------------------------------------
+pause
+goto menu
+
+:Ddosser
+@echo off
+mode 10
+title Ddos Tool
+color 0a
+cls
+echo.
+echo ========================================================
+echo           THE FIRST PING IS A CONNECTION TEST
+echo ========================================================
+echo.
+set /p x=Server Target:
+echo.
+echo ========================================================
+ping %x%
+echo ========================================================
+@ping.exe 127.0.0.1 -n 5 -w 1000 > nul
+goto Next
+:Next
+echo.
+echo.
+set /p m=Ip Address [Ip Host]:
+echo.
+set /p n=Packet Size:
+echo.
+set /p n=Server Downloads:
+echo.
+set /p n=Message To Server Owner:
+echo %ip% Your ip is changed before the attack is started
+echo.
+echo Is this the Ip Address you want to be attacking
+echo Attacking Server %m% 
+ping %m% -i %n% -t >nul
+echo Click Enter If so
+set /p input=
+echo ========================================================
+echo                    Attack Address
+echo ========================================================
+echo.
+:DDOS 
+color 0c 
+echo Attacking Server %m%
+ping %m% -i %n% -t >nul
+goto DDOS
+
+:Shutdown
+title Remote Computer Shutdown
+cls
+shutdown -i
+goto menu
